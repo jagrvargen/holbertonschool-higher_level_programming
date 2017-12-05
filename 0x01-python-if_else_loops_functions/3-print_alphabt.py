66c5213892299f8352805d990c6dda1f66b32b61
jagrvargen <229@holbertonschool.com>
2017-12-05 17:54:05 +0000

    changed code to use ascii values and continue statements

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-for l in 'abcdfghijklmnoprstuvwxyz':
-    print('{}'.format(l), end='')
+for l in range(97, 123):
+    if l == 101:
+        continue
+    elif l == 113:
+        continue
+    print('{}'.format(chr(l)), end='')
