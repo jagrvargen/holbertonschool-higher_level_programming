af27bd351874441499f304debb2a358d607cca13
jagrvargen <229@holbertonschool.com>
2017-12-06 01:15:12 +0000

    fixed incorrect negative return value

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 if number < 0:
     number = -number
-    digit = number % 10
+    digit = (number % 10) * -1
     number = -number
 else:
     digit = number % 10
