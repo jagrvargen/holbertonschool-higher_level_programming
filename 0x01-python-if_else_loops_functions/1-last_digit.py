d6f3cada5dfe7c0d3731e77cb78e45a819344e46
jagrvargen <229@holbertonschool.com>
2017-12-06 00:34:35 +0000

    corrected print statement for case 0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,6 +6,6 @@ print('Last digit of', number, 'is', digit, end=' ')
 if digit > 5:
     print('and is greater than 5')
 elif digit == 0:
-    print('and is zero')
+    print('and is 0')
 else:
     print('and is less than 6 and not 0')
