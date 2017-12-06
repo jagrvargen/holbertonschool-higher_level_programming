d46f02ac8e511c8a1426a731b4ad57420c868dbf
jagrvargen <229@holbertonschool.com>
2017-12-06 01:19:48 +0000

    rearranged character conversion functions

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,10 +2,9 @@
 def uppercase(str):
     ### Prints a string in uppercase ###
     for i in range(len(str)):
-        c = chr(ord(str[i]))
+        c = ord(str[i])
         if ord(str[i]) >= 97 and ord(str[i]) <= 122:
-            c = chr(ord(str[i]) - 32)
-        if i < len(str) - 1:
-            print('{}'.format(c), end='')
-        else:
-            print('{}'.format(c))
+            c = c - 32
+        if i < len(str):
+            print('{}'.format(chr(c)), end='')
+    print('')
