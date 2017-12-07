88947c0b1b36f7def8f016f9d836aa1a475b46a9
jagrvargen <jagarthewolf@gmail.com>
2017-12-07 21:27:24 +0000

    fixed argument count error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
         print('1 argument:')
         print('1: {}'.format(sys.argv[1]))
     elif len(sys.argv) > 2:
-        print('{} arguments:'.format(len(sys.argv)))
+        print('{} arguments:'.format(len(sys.argv) - 1))
         for i in range(1, len(sys.argv)):
             print('{}: {}'.format(i, sys.argv[i]))
     else:
