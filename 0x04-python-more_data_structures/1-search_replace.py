0aef750bd56bc9887c43cebd78e65f4cc740fedb
jagrvargen <jagarthewolf@gmail.com>
2017-12-14 18:15:14 +0000

    added file 1-search_replace.py with readme update

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if my_list:
+        newList = [num for num in my_list]
+        for i in newList:
+            if i == search:
+                i = replace
+        return newList
--- 0x04-python-more_data_structures/README.md
+++ 0x04-python-more_data_structures/README.md
@@ -1,2 +1,4 @@
 			Python - More Data Structures
 0. 0-square_matrix_simple.py - Computes the square value of all integers in a matrix.
+
+1. 1-search_replace.py - Replaces all occurences of an element by amother in a new list.
