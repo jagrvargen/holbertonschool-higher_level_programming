8aad975af6ccb40654a287213ef9748e2610ed4e
jagrvargen <jagarthewolf@gmail.com>
2017-12-14 23:48:39 +0000

    made null set return as only else case

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -3,5 +3,4 @@ def common_elements(set_1, set_2):
     if set_1 and set_2:
         shared = list(set_1 & set_2)
         return shared
-    elif set_1 or set_2:
-        return set()
+    return set()
