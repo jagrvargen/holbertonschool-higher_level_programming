3936d8b807eb78b65b9ed134432799e4f14adfe2
jagrvargen <jagarthewolf@gmail.com>
2017-12-14 23:43:11 +0000

    added check for 1 null set

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    if set_1 and set_2:
+        shared = list(set_1 & set_2)
+        return shared
+    elif set_1:
+        return set_1
+    elif set_2:
+        return set_2
