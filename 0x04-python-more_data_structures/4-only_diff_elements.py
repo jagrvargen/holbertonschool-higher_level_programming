e2a9b4d04ed5bd2d4e47480371a264ded78eccdc
jagrvargen <jagarthewolf@gmail.com>
2017-12-14 23:51:31 +0000

    added cases for only one set

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,4 +2,8 @@
 def only_diff_elements(set_1, set_2):
     if set_1 and set_2:
         return set_1 ^ set_2
+    elif set_1:
+        return set_1
+    elif set_2:
+        return set_2
     return set()
