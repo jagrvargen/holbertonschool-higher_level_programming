09940fbefe013b6de1a0769102659a41ffff8e02
jagrvargen <jagarthewolf@gmail.com>
2018-01-02 22:23:02 +0000

    added file 0-safe_print_list.py with readme

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for l in range(x):
+            print("{}".format(my_list[l]), end="")
+            count += 1
+    finally:
+        print()
+        return count
