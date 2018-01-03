d6bb3f59b8c2ce759b778d712162d4214c453ddd
jagrvargen <jagarthewolf@gmail.com>
2018-01-03 03:35:41 +0000

    added required except statement

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,6 +7,7 @@ def safe_print_list(my_list=[], x=0):
         for l in range(x):
             print("{}".format(my_list[l]), end="")
             count += 1
-    finally:
-        print()
-        return count
+    except:
+        pass
+    print()
+    return count
