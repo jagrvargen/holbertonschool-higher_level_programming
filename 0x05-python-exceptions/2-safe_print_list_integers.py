f3e026dfd114fecd04afd25c35add2ec59aade87
jagrvargen <jagarthewolf@gmail.com>
2018-01-03 00:57:18 +0000

    added file 2-safw_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        count = 0
+
+        for l in range(x):
+            if type(my_list[l]) == int:
+                print("{:d}".format(my_list[l]), end="")
+                count += 1
+        print()
+        return count
+    except (TypeError, ValueError):
+        pass
