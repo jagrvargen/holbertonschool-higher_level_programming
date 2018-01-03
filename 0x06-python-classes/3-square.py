29178dd2ced503693c95832803046ad854d81f17
jagrvargen <jagarthewolf@gmail.com>
2018-01-03 22:18:03 +0000

    added file 3-square.py with readme update

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """An empty class that defines a square"""
+
+    def __init__(self, size=0):
+        """Instantiates Square class with a size attribute
+        Args:
+            size (int): An integer denoting size.
+        """
+        if type(size) != int:
+            print("size must be an integer", end="")
+            raise TypeError
+        elif size < 0:
+            print("size must be >= 0", end="")
+            raise ValueError
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -2,3 +2,7 @@
 0. 0-square.py - An empty class that defines a square.
 
 1. 1-square.py - An instantiation of a Square with the private attribute size.
+
+2. 2-square.py - Defines a Square by a private instance attribute size, optional size which must be an integer of raise a TypeError, or if less than 0 raises a ValueError.
+
+3. 3-square.py - Adds a method which returns the current square's area.
