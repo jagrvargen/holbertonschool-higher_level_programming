89489f5732cd9759afa31828b7af8c5f568adf9b
jagrvargen <jagarthewolf@gmail.com>
2018-01-04 19:10:30 +0000

    added missing instance reference to print method

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -67,7 +67,7 @@ class Square:
         if self.size == 0:
             print()
         else:
-            if position[1] > 0:
+            if self.position[1] > 0:
                 for i in range(self.position[1]):
                     print(' ')
             for i in range(self.size):
