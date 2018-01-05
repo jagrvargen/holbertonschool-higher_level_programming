44367042d11dbeb3b4048fbdb0881799aab8ba23
jagrvargen <jagarthewolf@gmail.com>
2018-01-05 22:35:27 +0000

    added files 0-add_integer.py test/0-add_integer.txt and readme

---
+++ 0x07-add_integer.py/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def add_integer(a, b):
+    """ A function which adds two integers. """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
---
+++ 0x07-add_integer.py/README.md
@@ -0,0 +1,2 @@
+			Python Test Driven Development
+0. 0-add_integer.py, tests/0-add_integer.txt - A function that adds two integers (or floats) and a corresponding text file with associated tests.
