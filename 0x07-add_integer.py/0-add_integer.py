5eed1f88f82435fc08d4d888d807b682dd19efb8
jagrvargen <jagarthewolf@gmail.com>
2018-01-06 04:17:04 +0000

    added module docstring

--- 0x07-add_integer.py/0-add_integer.py
+++ 0x07-add_integer.py/0-add_integer.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+"""A module containing an addition function.
 
+   a (int) | (float) : An integer or floating point value.
+   b (int) | (float) : AN integer or floating point value.
+"""
 
 def add_integer(a, b):
-    """ A function which adds two integers. """
+    """
+       A function which adds two integers.
+    """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     elif not isinstance(b, int) and not isinstance(b, float):
