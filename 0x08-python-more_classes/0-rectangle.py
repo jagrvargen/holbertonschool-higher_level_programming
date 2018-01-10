24a2a4b13602fbd964634b9db1112157da0a875b
jagrvargen <jagarthewolf@gmail.com>
2018-01-10 19:54:47 +0000

    added file rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for definition of rectangle class.
+
+A module containing a class definition for a printable rectangle.
+
+"""
+class Rectangle:
+    """A class which creates a rectangle."""
+    pass
