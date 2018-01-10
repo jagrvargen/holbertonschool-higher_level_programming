6e506edd0cdeb0aae448959e3a11a624803cce5c
jagrvargen <jagarthewolf@gmail.com>
2018-01-10 21:37:21 +0000

    added docstrings

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,15 +8,18 @@ class Rectangle:
     """A class which creates a rectangle."""
 
     def __init__(self, width=0, height=0):
+        """The __init__ method creates a new instance of a Rectangle"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """The width getter returns the value of a Rectangle's width."""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """The width setter sets the value of a Rectangles's width."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -25,10 +28,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """The height getter returns the height of a Rectangle's height."""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """The height setter sets the value of a Rectangle's height."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
