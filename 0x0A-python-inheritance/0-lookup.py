2cd8633f31cc6942b9dcf0db1b9a42a46e7ad129
jagrvargen <jagarthewolf@gmail.com>
2018-01-16 19:18:40 +0000

    added 0-lookup.py and readme files

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def lookup(obj):
+    """A function which returns the list of attributes
+       and methods of an object.
+    """
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,3 @@
+				Python Inheritance
+0. 0-lookup.py - A function that returns the list of available attributes and methods of an object.
+
