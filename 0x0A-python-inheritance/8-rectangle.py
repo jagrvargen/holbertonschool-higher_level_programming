28947a1b02bbde62d90f7df687aeff549ed96df0
jagrvargen <jagarthewolf@gmail.com>
2018-01-16 23:45:32 +0000

    corrected error message

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -21,14 +21,9 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """A method which raises exceptions if incorrect types are passed"""
         if type(value) != int:
-            raise TypeError("<name> must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
-            raise ValueError("<name> must be greater than 0")
-
-
-"""A module which contains the class definition for Rectangle.
-
-"""
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
