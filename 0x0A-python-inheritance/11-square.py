8a2d999af6f05127267b728dce04b0c9cc67db0a
jagrvargen <jagarthewolf@gmail.com>
2018-01-17 00:24:51 +0000

    added file 11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""A module which contains an empty class BaseGeometry.
+
+
+"""
+
+
+class BaseGeometry:
+    """A class which contains the public instance method area() which
+       raises an Exception.
+    """
+
+    def __init__(self):
+        """Initializes an instance of a BaseGeometry object."""
+        self.__self = self
+
+    def area(self):
+        """Raises an Exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """A method which raises exceptions if incorrect types are passed"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Class definition for Rectangle object which inherits from
+       BaseGeometry class.
+    """
+
+    def __init__(self, width, height):
+        """Instantiates an instance of a Rectangle object"""
+        self.__width = width
+        self.__height = height
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def __str__(self):
+        """Returns a description of a Rectangle object instance."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        """Returns the area of the Rectangle object."""
+        return self.__width * self.__height
+
+
+class Square(Rectangle):
+    """Class definition for a Square object which inherits from the Rectangle
+       class.
+    """
+
+    def __init__(self, size):
+        """Instantiates an instance of a Square object."""
+        Rectangle.__init__(self, size, size)
+        self.__size = size
+        self.integer_validator("size", size)
+        self.area()
+
+    def __str__(self):
+        """Returns a description of a Square object instance."""
+        return "[Square] {}/{}".format(self.__size, self.__size)
