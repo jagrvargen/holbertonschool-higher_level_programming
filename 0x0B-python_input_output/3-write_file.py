090b114e864b0a13e865d38166c29dcf3270201a
jagrvargen <jagarthewolf@gmail.com>
2018-01-17 22:32:29 +0000

    removed extraneous loop

--- 0x0B-python_input_output/3-write_file.py
+++ 0x0B-python_input_output/3-write_file.py
@@ -6,9 +6,7 @@ def write_file(filename="", text=""):
        Writes a string to a text file (utf-8) and returns the number of
        characters written.
     """
-    word_count = 0
     with open(filename, mode='w', encoding="utf-8") as fp:
-        for c in range(len(text)):
             fp.write(text)
-            word_count += 1
-    return word_count
+    char_count = len(text)
+    return char_count
