f5a3e87d983a7e3e8f7e4dbffbbc3dc4ad9a68a3
jagrvargen <jagarthewolf@gmail.com>
2018-01-18 16:45:32 +0000

    fixed pep8 check

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
    This module contains a function which reads and prints a text file.
 """
 
+
 def read_file(filename=""):
     """Reads a text file (utf-8) and prints it to stdout"""
     with open(filename, encoding="utf-8") as fp:
