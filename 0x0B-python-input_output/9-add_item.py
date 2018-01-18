64edcfc2eac95fdc846fae758a9a3be9e60ad06c
jagrvargen <jagarthewolf@gmail.com>
2018-01-18 22:59:10 +0000

    corrected argv format

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -3,6 +3,8 @@
    This module contains a script to add all arguments to a Python list,
    and then save them to a file.
 """
+
+
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 import sys
@@ -11,11 +13,11 @@ import os
 
 
 arguments = [item for item in sys.argv[1:]]
-if os.path.isfile('./add_item.json'):
+if os.path.isfile('add_item.json'):
     my_obj = load_from_json_file('add_item.json')
     for item in arguments:
         my_obj.append(item)
     save_to_json_file(my_obj, 'add_item.json')
 else:
     with open('add_item.json', 'w', encoding="utf-8") as fp:
-        fp.write('[]')
+        fp.write(''.join(arguments))
