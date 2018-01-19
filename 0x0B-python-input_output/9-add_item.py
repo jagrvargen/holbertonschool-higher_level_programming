f0bec7052f3a9c30c45f0047a61fc85dfba7b132
jagrvargen <jagarthewolf@gmail.com>
2018-01-19 00:44:44 +0000

    fixed object dump

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -9,15 +9,14 @@ save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 import sys
 import json
-import os
 
 
 arguments = [item for item in sys.argv[1:]]
-if os.path.isfile('add_item.json'):
+try:
     my_obj = load_from_json_file('add_item.json')
     for item in arguments:
         my_obj.append(item)
     save_to_json_file(my_obj, 'add_item.json')
-else:
+except FileNotFoundError:
     with open('add_item.json', 'w', encoding="utf-8") as fp:
-        fp.write(''.join(arguments))
+        json.dump(arguments, fp)
