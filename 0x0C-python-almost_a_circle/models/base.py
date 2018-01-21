6298a8ea1810dd030042caa339dae844f234df71
jagrvargen <jagarthewolf@gmail.com>
2018-01-21 23:18:13 +0000

    added the static method to_json_string() to the Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
    This module contains the class definition for Base. Which forms the base
    for all other classes in the 'almost a circle' project.
 """
+import json
 
 
 class Base:
@@ -19,3 +20,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns a JSON string representation of list_dictionaries."""
+        if not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
