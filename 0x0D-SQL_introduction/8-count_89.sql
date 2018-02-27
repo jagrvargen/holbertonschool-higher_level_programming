1eedcca73fa7526f82c33ef57f7880a0eb714e70
jagrvargen <jagarthewolf@gmail.com>
2018-02-27 03:16:01 +0000

    trying new formulation

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 -- Displays the number of records with id = 89 in first_table.
 -- Select and Count id = 89
-SELECT COUNT(`id` = 89) FROM `first_table`;
+SELECT COUNT(*) FROM `first_table` WHERE `id`= 89;
