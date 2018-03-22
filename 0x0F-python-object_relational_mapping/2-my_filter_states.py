2ea33b1a7ef708f1e0e8ef9a4bc48469b0079347
jagrvargen <jagarthewolf@gmail.com>
2018-03-22 20:59:04 +0000

    added file 2-my_filter_states.py with readme update

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+   This module contains a MySQLdb query.
+"""
+import MySQLdb
+from sys import argv
+
+
+USER = argv[1]
+PSWD = argv[2]
+DB = argv[3]
+NAME = argv[4]
+
+db = MySQLdb.connect(user=USER, host="localhost", db=DB, passwd=PSWD)
+cur = db.cursor()
+
+cur.execute("""SELECT * FROM states WHERE name = %s ORDER BY id ASC""", [NAME])
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
+
+cur.close()
+db.close()
--- 0x0F-python-object_relational_mapping/README.md
+++ 0x0F-python-object_relational_mapping/README.md
@@ -3,3 +3,5 @@
 0. 0-select_states.py - Lists all 'states' from the database hbtn_0e_0_usa.
 
 1. 1-filter_states.py - Lists all states with a name starting with N from the database hbtn_0e_0_usa.
+
+2. 2-my_filter_states.py - Displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
