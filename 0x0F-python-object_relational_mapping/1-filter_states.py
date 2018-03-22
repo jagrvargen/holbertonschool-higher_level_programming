ae6c86590c221538ca7d278f0031d7e71d055748
jagrvargen <jagarthewolf@gmail.com>
2018-03-22 20:42:23 +0000

    added file 1-filter_states.py with readme update

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+   This module contains a MySQLdb query.
+"""
+import MySQLdb
+from sys import argv
+
+
+USER = argv[1]
+PSWD = argv[2]
+DB = argv[3]
+
+db = MySQLdb.connect(user=USER, host="localhost", db=DB, passwd=PSWD)
+cur = db.cursor()
+
+cur.execute("""SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC""")
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
--- 0x0F-python-object_relational_mapping/README.md
+++ 0x0F-python-object_relational_mapping/README.md
@@ -1,3 +1,5 @@
 #Python ORM
 
 0. 0-select_states.py - Lists all 'states' from the database hbtn_0e_0_usa.
+
+1. 1-filter_states.py - Lists all states with a name starting with N from the database hbtn_0e_0_usa.
