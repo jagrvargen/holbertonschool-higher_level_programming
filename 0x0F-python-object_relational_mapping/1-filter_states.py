7c469608760edc06a5a5782c69e90c4b81b53957
jagrvargen <jagarthewolf@gmail.com>
2018-03-23 17:33:16 +0000

    check for uppercase added

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -18,7 +18,8 @@ if __name__ == "__main__":
     rows = cur.fetchall()
 
     for row in rows:
-        print(row)
+        if row[1][0].isupper():
+            print(row)
 
     cur.close()
     db.close()
