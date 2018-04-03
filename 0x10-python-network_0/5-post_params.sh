a609110773d175da5245761494ce4dd390b04751
jagrvargen <jagarthewolf@gmail.com>
2018-04-03 22:23:55 +0000

    removed unnecessary + symbols

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a POST request to a URL adding email and subject data.
-curl -X POST "$1" -F "email=hr@holbertonschool.com" -F "subject=I+will+always+be+here+for+PLD" 
+curl -X POST "$1" -F "email=hr@holbertonschool.com" -F "subject=I will always be here for PLD" 
