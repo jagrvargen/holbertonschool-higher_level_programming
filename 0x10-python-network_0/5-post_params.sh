71403fbcaa0436a4a685d9bfca38c8d9e3317570
jagrvargen <jagarthewolf@gmail.com>
2018-04-03 22:10:42 +0000

    rearranged arguments to correcy order

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Sends a POST request to a URL adding email and subject data.
-curl -d "email=hr@holbertonschool.com&subject=I+will+always+be+here+for+PLD"\
--X POST "$!"
+curl -X POST "$1" -d "email=hr@holbertonschool.com&subject=I+will+always+be+here+for+PLD" 
