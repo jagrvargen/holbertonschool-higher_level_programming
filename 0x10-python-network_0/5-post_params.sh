15f8f226e759368d2b2b9da967392a0386afcfb9
jagrvargen <jagarthewolf@gmail.com>
2018-04-03 22:32:03 +0000

    trying without POST

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a POST request to a URL adding email and subject data.
-curl -X POST "$1" -ds "email=hr@holbertonschool.com&subject=I will always be here for PLD" 
+curl -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
