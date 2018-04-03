2a97d8489014f22ce3b9572a716a81d9f6f28c31
jagrvargen <jagarthewolf@gmail.com>
2018-04-03 21:36:05 +0000

    removed GET

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request to a URL with a header variable to retrieve body.
-curl -H "X-HolbertonSchool-User-Id: 98" -X GET "$1"
+curl -H "X-HolbertonSchool-User-Id: 98" "$1"
