2096c518616259eefba1b976a67077c293ea9cb2
jagrvargen <jagarthewolf@gmail.com>
2018-04-10 22:31:34 +0000

    initial commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+   A script which fetches the URL https://intranet.hbtn.io/status
+"""
+import urllib.request
+import urllib.parse
+
+
+req = urllib.request.Request("https://intranet.hbtn.io/status")
+
+with urllib.request.urlopen(req) as response:
+    html = response.read()
+
+print("Body response:")
+print("\t- type: {}".format(type(html)))
+print("\t- content: {}".format(html))
+print("\t- utf8 content: {}".format(html.decode("utf-8")))
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,3 @@
+#Python Network - 1
+
+0. 0-hbtn_status.py - A Python script that fetches https://intranet.hbtn.io/status using the urllib library.
