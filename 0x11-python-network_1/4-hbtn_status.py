3fa61d7a1651ee5c2e6f6bf50a42b4c9b7499482
jagrvargen <jagarthewolf@gmail.com>
2018-04-12 05:04:49 +0000

    corrected error message spelling

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     import requests
 
     url = "https://intranet.hbtn.io/status"
-    r = requests.get(url)
 
-    print("Body reponse:")
+    r = requests.get(url)
+    print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
