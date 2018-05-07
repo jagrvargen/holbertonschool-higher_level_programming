7767d4472c292afaf84c8f2d15098c518a64f1ea
jagrvargen <jagarthewolf@gmail.com>
2018-05-07 23:43:50 +0000

    removed unnecessary constructor

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,16 +3,12 @@
 const square = require('./5-square.js');
 
 module.exports = class Square extends square {
-  constructor (size) {
-    super(size);
-    this.size = size;
-  }
   charPrint (c) {
     if (!c) {
       super.print();
     } else {
-      for (let i = 0; i < this.size; i++) {
-        console.log(String(c).repeat(this.size));
+      for (let i = 0; i < this.height; i++) {
+        console.log(String(c).repeat(this.height));
       }
     }
   }
