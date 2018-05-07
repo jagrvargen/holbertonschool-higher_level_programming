191cf13f540903c52202b1bfc1d3edf2b1d03bc7
jagrvargen <jagarthewolf@gmail.com>
2018-05-07 23:49:14 +0000

    removed built in reverse call as per instructions

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return list.reverse();
+  myList = [];
+
+  for (let i = list.length - 1, j = 0; i >= 0; i--, j++) {
+    myList[j] = list[i];
+  }
+  return myList;
 };
