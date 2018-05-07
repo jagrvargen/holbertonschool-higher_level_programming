ab01cad1f918f5276a5a48d4b93ece1717cd467c
jagrvargen <jagarthewolf@gmail.com>
2018-05-07 22:22:55 +0000

    fixed semistandard errors

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-  function myConverter(num) {
+  function myConverter (num) {
     return num.toString(base);
   }
   return myConverter;
-}
+};
