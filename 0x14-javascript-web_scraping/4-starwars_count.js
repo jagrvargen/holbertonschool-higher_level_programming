5fce40db45742d962730e831fb4313c627600eb9
jagrvargen <jagarthewolf@gmail.com>
2018-05-08 23:28:00 +0000

    adding catch for response

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,13 +4,13 @@ const url = process.argv[2];
 const wedge = 'https://swapi.co/api/people/18/';
 const request = require('request');
 
-request(url, function (error, body) {
+request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
     const res = JSON.parse(body['body']);
     let count = 0;
-    const results = res['results'];
+    const results = res['body'];
     for (let i = 0; i < results.length; i++) {
       for (let j = 0; j < results[i]['characters'].length; j++) {
         if (results[i]['characters'][j] === wedge) {
