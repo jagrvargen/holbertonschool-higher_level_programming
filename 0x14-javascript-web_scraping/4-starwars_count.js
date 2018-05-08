b1ea6b31f1f90b8d4f301f5929b58a061abb233e
jagrvargen <jagarthewolf@gmail.com>
2018-05-08 15:57:21 +0000

    added file 4-starwars_count.js with readme update

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const url = process.argv[2];
+const wedge = 'https://swapi.co/api/people/18/';
+const request = require('request');
+
+request(url, function (error, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const res = JSON.parse(body['body']);
+    let count = 0;
+    const results = res['results'];
+    for (let i = 0; i < results.length; i++) {
+      for (let j = 0; j < results[i]['characters'].length; j++) {
+        if (results[i]['characters'][j] === wedge) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
--- 0x14-javascript-web_scraping/README.md
+++ 0x14-javascript-web_scraping/README.md
@@ -7,3 +7,5 @@
 2. 2-statuscode.js - A script that displays the status code of a GET request.
 
 3. 3-starwars_title.js - A script that prints the title of a Star Wars movie where the episode number matches a given integer.
+
+4. 4-starwars_count.js - A script that prints the number of movies where the character Wedge Antilles is present.
