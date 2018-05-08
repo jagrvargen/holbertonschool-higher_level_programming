521b03d2f969962c3c0c4e3bc257d95621855e92
jagrvargen <jagarthewolf@gmail.com>
2018-05-08 23:34:29 +0000

    fixed broken fixes

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,7 +4,7 @@ const url = process.argv[2];
 const wedge = 'https://swapi.co/api/people/18/';
 const request = require('request');
 
-request(url, function (error, response, body) {
+request(url, function (error, body) {
   if (error) {
     console.log(error);
   } else {
