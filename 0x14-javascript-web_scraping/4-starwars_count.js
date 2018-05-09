0eadd673d772c03d9d246b0e25f17b68e6f63c7d
jagrvargen <jagarthewolf@gmail.com>
2018-05-09 00:21:46 +0000

    refactored code to match reqs

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,13 +1,23 @@
 #!/usr/bin/node
 
+const url = process.argv[2];
 const wedge = 'https://swapi.co/api/people/18/';
 const request = require('request');
 
-request(wedge, function (error, response, body) {
+request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    let films = JSON.parse(body).films;
-    console.log(films.length);
+    let films = JSON.parse(body).results;
+    let count = 0;
+    for (let i = 0; i < films.length; i++) {
+      let chars = films[i]['characters'];
+      for (let j = 0; j < chars.length; j++) {
+        if (wedge === chars[j]) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
   }
 });
