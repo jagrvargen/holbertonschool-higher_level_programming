41c527ac23a43dddc26383b7d017212903bf0fc0
jagrvargen <jagarthewolf@gmail.com>
2018-05-09 00:09:51 +0000

    fixed return body

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,11 +3,11 @@
 const wedge = 'https://swapi.co/api/people/18/';
 const request = require('request');
 
-request(wedge, function (error, body) {
+request(wedge, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    let films = JSON.parse(body['body']).films;
+    let films = JSON.parse(body).films;
     console.log(films.length);
   }
 });
