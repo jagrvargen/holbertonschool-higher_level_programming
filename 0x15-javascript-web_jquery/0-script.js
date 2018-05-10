9e7c2643d9141a30fee976db0ad272091985d11d
jagrvargen <jagarthewolf@gmail.com>
2018-05-10 17:37:50 +0000

    fixed semistandard errors

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
   changeColor();
 });
 
-function changeColor() {
+function changeColor () {
   const header = document.querySelector('header');
   header.style.color = '#FF0000';
-};
+}
